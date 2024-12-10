7a5900c5be0d82d0dc7780c12ed9403d44bfb649
Lawando69 <lawand15.kong@gmail.com>
2024-12-10 12:57:52 +0000

    Implemented the add function to post and append information onto the json file

---
+++ routes/workout_routes.py
@@ -0,0 +1,35 @@
+import json
+from typing import List
+from fastapi import APIRouter
+from pydantic import BaseModel
+
+
+workout_routes = APIRouter()
+
+@workout_routes.get("/")
+def get_all_workouts():
+    with open("./fake_db/workouts.json", "r") as file: 
+        return json.load(file)
+    
+
+def all_workouts():
+    with open("./fake_db/workouts.json", "r") as file:
+        return get_all_workouts()
+    
+class Workout(BaseModel):
+    name: str
+    exercises: List[str]
+
+
+@workout_routes.post("/")
+def add_workout(new_workout: Workout):
+    all_workouts = get_all_workouts()
+
+    all_workouts.append(
+        new_workout.model_dump()
+    )
+
+    with open("./fake_db/workouts.json", "w") as file:
+        file.write(json.dumps(all_workouts, indent=4))
+    
+    return all_workouts
